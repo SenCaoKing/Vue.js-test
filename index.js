7960d6610ab93410a2c59d770c9cf48b1a5f2536
KingCao <1134349927@qq.com>
2018-07-19 10:08:32 +0800

    vue声明式渲染之绑定元素

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// 声明式渲染
+/* 声明式渲染 */
+// ① 文本插值
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
     }
+});
+
+// ② 绑定元素特性
+var app2 = new Vue({
+	el: '#app-2',
+	data: {
+		message: '页面加载于' + new Date().toLocaleString()
+	}
 });
